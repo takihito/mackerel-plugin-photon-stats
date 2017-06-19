9b845d95ec396236422d8b66afeded65006f59c4
akihito <takeda.akihito@gmail.com>
2017-06-19 17:33:10 +0900

    initial commit

--- lib/photon-stats.go
+++ lib/photon-stats.go
@@ -1,17 +1,23 @@
 package photonstats
 
 import (
+	"flag"
+	"fmt"
 	mp "github.com/mackerelio/go-mackerel-plugin-helper"
+	"io/ioutil"
+	"net/http"
 )
 
 const (
-	region = "ja-jp"
+	photonUrl    = "https://counter.photonengine.com/Counter/api/data/app/"
+	photonRegion = "jp"
 )
 
 type PhotonStatsPlugin struct {
 	Url    string
 	AppId  string
 	Region string
+	Token  string
 }
 
 var graphdef = map[string]mp.Graphs{
@@ -19,22 +25,51 @@ var graphdef = map[string]mp.Graphs{
 		Label: "Photon Rooms",
 		Unit:  "integer",
 		Metrics: []mp.Metrics{
-			{Name: "rooms", Label: "count", Diff: false},
+			{Name: "rooms", Label: "room", Diff: false},
 		},
 	},
 	"photon.stats": {
 		Label: "Photon Stats",
 		Unit:  "integer",
 		Metrics: []mp.Metrics{
-			{Name: "ccu", Label: "count", Diff: false},
-			{Name: "rejects", Label: "count", Diff: false},
+			{Name: "ccu", Label: "ccu", Diff: false},
+			{Name: "rejects", Label: "reject", Diff: false},
 		},
 	},
 }
 
+func getPhotonStats(url string, appId string, region string, token string, name string) (string, error) {
+	apiUrl := url + appId + "/" + region + "/" + name
+	req, err := http.NewRequest("GET", apiUrl, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Authorization", token)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("HTTP status error: %d", resp.StatusCode)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return "", err
+	}
+	return string(body[:]), nil
+}
+
 // FetchMetrics interface for mackerelplugin
 func (u PhotonStatsPlugin) FetchMetrics() (stats map[string]interface{}, err error) {
-	return map[string]interface{}{"rooms": 100, "ccu": 20, "reject": 5}, nil
+	ccu, err := getPhotonStats(u.Url, u.AppId, u.Region, u.Token, "ccu")
+	rooms, err := getPhotonStats(u.Url, u.AppId, u.Region, u.Token, "rooms")
+	rejects, err := getPhotonStats(u.Url, u.AppId, u.Region, u.Token, "rejects")
+	if err != nil {
+		return nil, err
+	}
+	return map[string]interface{}{"rooms": rooms, "ccu": ccu, "rejects": rejects}, nil
 }
 
 // GraphDefinition interface for mackerelplugin
@@ -43,7 +78,18 @@ func (u PhotonStatsPlugin) GraphDefinition() map[string](mp.Graphs) {
 }
 
 func Do() {
+	optAppid := flag.String("appid", "", "App Id")
+	optUrl := flag.String("url", photonUrl, "Photon analytivs api url")
+	optRegion := flag.String("region", photonRegion, "region")
+	optToken := flag.String("token", "", "Authorization Token")
+	flag.Parse()
+
 	var photon PhotonStatsPlugin
+	photon.AppId = *optAppid
+	photon.Url = *optUrl
+	photon.Region = *optRegion
+	photon.Token = *optToken
+
 	helper := mp.NewMackerelPlugin(photon)
 	helper.Run()
 }
--- main.go
+++ main.go
@@ -3,6 +3,5 @@ package main
 import "github.com/takihito/mackerel-plugin-photon-stats/lib"
 
 func main() {
-	//	fmt.Print("Hello world!")
 	photonstats.Do()
 }
