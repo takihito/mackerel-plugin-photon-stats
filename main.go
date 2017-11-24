fef10549376101f801da70cbd96b44fe7a6279cd
akihito <takeda.akihito@gmail.com>
2017-11-24 16:55:15 +0900

    getPhotonStatsをPhotonStatsPlugin配下に移動

--- main.go
+++ main.go
@@ -1,25 +1,11 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
-
 	"github.com/takihito/mackerel-plugin-photon-stats/lib"
 )
 
 const version = "0.0.4"
 
-var (
-	showVersion = flag.Bool("version", false, "show version")
-)
-
 func main() {
-	flag.Parse()
-	if *showVersion {
-		fmt.Printf("version: %s \n", version)
-		os.Exit(0)
-	}
-
 	photonstats.Do()
 }
